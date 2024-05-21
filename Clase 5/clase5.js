a0e6c92b110471d88721360da9d8ba5b2e6da834
Agustin Bustamante <144798158+lagubusta@users.noreply.github.com>
2024-05-21 16:34:19 +0000

    Eje complementarios

--- Clase 5/clase5.js
+++ Clase 5/clase5.js
@@ -75,7 +75,7 @@ console.log(Producto);
 const Leche = new Producto("Leche", 1500, 10,)
 console.log(Leche);
 
-// Crea un constructor llamado Persona con las propiedades nombre, edad y ciudad. Crea un objeto nuevo utilizando este constructor.
+// 6- Crea un constructor llamado Persona con las propiedades nombre, edad y ciudad. Crea un objeto nuevo utilizando este constructor.
 function Persona(nombre, edad, ciudad,) {
     this.nombre = nombre;
     this.edad = edad;
@@ -87,7 +87,7 @@ console.log(Agustin);
 const Sofia = new Persona("Sofia", 24, "Buenos Aires");
 console.log(Sofia);
 
-//----------------------Crea un constructor llamado Libro con las propiedades titulo, autor y año. Crea un objeto nuevo utilizando este constructor.
+// 7- Crea un constructor llamado Libro con las propiedades titulo, autor y año. Crea un objeto nuevo utilizando este constructor.
 function Libro(titulo, autor, año) {
     this.titulo = titulo;
     this.autor = autor;
@@ -97,8 +97,8 @@ console.log(Libro);
 const HarryPotter = new Libro("HP la pierda", "JK", 2001);
 console.log(HarryPotter);
 
-//----------------------USO DE THIS ----------------------
-//----------------------Crea un objeto llamado rectangulo con las propiedades base y altura. Agrega un método llamado calcularArea que calcule y devuelva el área del rectángulo.
+//-----------------------------------USO DE THIS
+// 8- Crea un objeto llamado rectangulo con las propiedades base y altura. Agrega un método llamado calcularArea que calcule y devuelva el área del rectángulo.
 function Rectangulo(base, altura) {
     this.base = base;
     this.altura = altura;
@@ -110,7 +110,7 @@ function Rectangulo(base, altura) {
 const rectangulo = new Rectangulo(5, 10);
 console.log(rectangulo.calcularArea());
 
-//----------------------Crea un objeto llamado circulo con la propiedad radio. Agrega un método llamado calcularPerimetro que calcule y devuelva el perímetro del círculo.
+// 9- Crea un objeto llamado circulo con la propiedad radio. Agrega un método llamado calcularPerimetro que calcule y devuelva el perímetro del círculo.
 function Circulo(radio) {
     this.radio = radio;
 
@@ -121,7 +121,7 @@ function Circulo(radio) {
 const circulo = new Circulo (5);
 console.log(circulo.calcularPerimetro());
 
-//----------------------Crea un objeto llamado estudiante con las propiedades nombre, edad y curso. Agrega un método llamado presentarse que imprima en la consola una presentación del estudiante.
+// 10- Crea un objeto llamado estudiante con las propiedades nombre, edad y curso. Agrega un método llamado presentarse que imprima en la consola una presentación del estudiante.
 
 function Estudiante (nombre, edad, curso){
     this.nombre = nombre;
@@ -135,4 +135,24 @@ function Estudiante (nombre, edad, curso){
 const estudiante = new Estudiante ("Alejandro", 35, "1 A");
 console.log (Estudiante.Alejandro);
 
-console.log ("Agustin lista");
+//-----------------------------------Métodos y Operaciones con Objetos
+
+// 11- Crea un objeto llamado cuentaBancaria con las propiedades saldo y titular. Agrega un método llamado depositar que reciba un monto y lo sume al saldo.
+function CuentaBancaria(titular, saldo){
+    this.titular = titular;
+    this.saldo = saldo;   
+    
+    this.depositar = function (monto){
+        this.saldo += monto;
+    }
+}
+const cuenta = new CuentaBancaria ("Agustin", 1000);
+cuenta.depositar(500);
+console.log(`Nuevo Saldo: ${cuenta.saldo}`);
+
+
+// 12- Agrega un método llamado retirar al objeto cuentaBancaria que reciba un monto y lo reste al saldo.
+
+
+// 13- Crea un objeto llamado agenda que contenga un array de contactos. Agrega un método llamado agregarContacto que reciba un objeto de contacto y lo añada al array.
+// 14- Agrega un método llamado buscarContacto al objeto agenda que reciba el nombre de un contacto y devuelva el objeto correspondiente.
---
+++ Complementarios/Ejercicio1.js
@@ -0,0 +1,69 @@
+//----------------------ACTIVIDAD 1
+
+// Crear tres (3) variables para asignar el nombre, apellido y edad de Homero, respectivamente. Es indistinto el empleo de var o let en la declaración.
+
+console.log("Crear tres (3) variables para asignar el nombre, apellido y edad de Homero, respectivamente. Es indistinto el empleo de var o let en la declaración.");
+
+let nombre = "Homero";
+let apellido = "Simpson";
+let edad = 38;
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+
+//----------------------ACTIVIDAD 2
+// Crear cinco (5) variables constantes, asignando a cada una de ellas diferentes nombres de ciudades. Generalmente, dichas variables se declaran con const.
+console.log("Crear cinco (5) variables constantes, asignando a cada una de ellas diferentes nombres de ciudades. Generalmente, dichas variables se declaran con const.");
+
+const ciudad1 = "Buenos Aires";
+const ciudad2 = "San Isidro";
+const ciudad3 = "Cañuelas";
+const ciudad4 = "Lobos";
+const ciudad5 = "Rio Cuarto";
+
+console.log(ciudad1);
+console.log(ciudad2);
+console.log(ciudad3);
+console.log(ciudad4);
+console.log(ciudad5);
+
+//----------------------ACTIVIDAD 3
+// Declarar variables para representar la información de un carnet de conducir.
+// Luego, concatenarlas y asignar el resultado de la operación a una variable denominada carnet.
+
+console.log("Declarar variables para representar la información de un carnet de conducir. Luego, concatenarlas y asignar el resultado de la operación a una variable denominada carnet.");
+
+nombre = "Bart";
+apellido = "Simpson";
+let fechaEdicion = "21/05/2024";
+const carnet = console.log("Nombre: " + nombre + " Apellido: " + apellido + " Fecha: " + fechaEdicion);
+
+//----------------------ACTIVIDAD 4
+// Solicitar al usuario cinco (5) nombres pertenecientes a integrantes de una familia. Luego, concatenar dichas entradas y efectuar una salida por alerta.
+
+// let integrante1 = prompt("Ingresa nombre:");
+// let integrante2 = prompt("Ingresa nombre:");
+// let integrante3 = prompt("Ingresa nombre:");
+// let integrante4 = prompt("Ingresa nombre:");
+// let integrante5 = prompt("Ingresa nombre:");
+
+// let familia =
+//     "1) " + integrante1 + " " +
+//     "2) " + integrante2 + " " +
+//     "3) " + integrante3 + " " +
+//     "4) " + integrante4 + " " +
+//     "5) " + integrante5;
+// console.log(familia);
+
+//----------------------ACTIVIDAD 5
+// Solicitar al usuario uno o más precios. Luego, efectuar un descuento en porcentajes (20%, 30%, etcétera) sobre los valores ingresados, y realizar una salida.
+
+let precio1 = prompt("Ingresa precio 1:");
+let precio2 = prompt("Ingresa precio 2:");
+
+let descuento1 = 0.2;
+let descuento2 = 0.3;
+
+console.log("Si a " + precio1 + " le hacemos " + descuento1 + " queda en " + precio1*descuento1);
+console.log("Si a " + precio2 + " le hacemos " + descuento2 + " queda en " + precio2*descuento2);
