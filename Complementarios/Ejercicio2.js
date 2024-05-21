8c76d611fafd8999e8558625651a1800ab5a9103
Agustin Bustamante <144798158+lagubusta@users.noreply.github.com>
2024-05-21 17:44:59 +0000

    Eje 2

--- Complementarios/Ejercicio2.js
+++ Complementarios/Ejercicio2.js
@@ -63,15 +63,20 @@
 // ----------------------ACTIVIDAD 5
 // Solicitar al usuario cuatro (4) productos de almacén. Si todos los elementos fueron cargados, realizar una única salida compuesta por el listado de productos. Caso contrario, la salida será “Error: Es necesario cargar todos los productos”.
 
-const NoVacio = "";
+// let producto1 = prompt("Ingresa producto1:");
+// let producto2 = prompt("Ingresa producto2:");
+// let producto3 = prompt("Ingresa producto3:");
+// let producto4 = prompt("Ingresa producto4:");
 
-let producto1 = prompt("Ingresa producto1:");
-let producto2 = prompt("Ingresa producto2:");
-let producto3 = prompt("Ingresa producto3:");
-let producto4 = prompt("Ingresa producto4:");
+// if (producto1 != "" && producto2 != "" && producto3 != "" && producto4 != ""){
+//     let heladera = 
+//     "1) " + preoducto1 + " " +
+//     "2) " + producto2 + " " +
+//     "3) " + producto3 + " " +
+//     "4) " + producto4;
+//     console.log (heladera);
+// } else {
+//     console.log("Error: Es necesario completar todos los productos");
+// }
 
-if (producto1 != "" && producto2 != "" && producto3 != "" && producto4 != ""){
-    
-}
-
-console.log(producto1 + " " + producto2 +" "+ producto3 +" "+producto4 );
+// console.log(producto1 + " " + producto2 +" "+ producto3 +" "+producto4 );
