44d059ae95846b0fbc8f963458adb62c6ebe225a
Agustin Bustamante <144798158+lagubusta@users.noreply.github.com>
2024-05-22 14:41:07 +0000

    Fin Eje5

--- Complementarios/Ejercicio5.js
+++ Complementarios/Ejercicio5.js
@@ -47,6 +47,9 @@
 // Luego invocar al menos un (1) objeto usando esta clase, y solicitar al usuario tres (3) horas. Informar por alerta si la tienda está abierta, en función de la hora ingresada.
 
 
+
+
+
 // class Tienda {
 //     constructor(nombre, direccion, propietario, rubro) {
 //         this.nombre = nombre;
@@ -75,3 +78,70 @@
 // ----------------------ACTIVIDAD 4
 // Declarar un método para la clase Tienda con la siguiente cabecera esPropietario(nombre). Se retorna true si el nombre enviado corresponde al propietario de la tienda.Caso contrario, se retorna false.
 // Luego, invocar al menos tres(3) objetos usando esta clase y solicitar al usuario cinco(5) nombres. Informar por alerta si los nombres pertenecen a algún dueño de tienda.
+
+// class Tienda {
+//     constructor(nombre, direccion, propietario, rubro) {
+//         this.nombre = nombre;
+//         this.direccion = direccion;
+//         this.propietario = propietario;
+//         this.rubro = rubro;
+//     }
+//     esPropietario(nombre) {
+//         return this.propietario == nombre;
+//     }
+// }
+// const tienda1 = new Tienda("Agustin", "Alberti 880", "Federico");
+// const tienda2 = new Tienda("JuaniJuana", "Gral Maria Paz 3970", "Chano");
+// const tienda3 = new Tienda("Vivero del puerto", "Tiscornia 1014", "Belu");
+// for (let i = 0; i < 5; i++) {
+//     let entrada = prompt("Ingrsa nombre de propietario: ");
+//     if (tienda1.esPropietario(entrada)) {
+//         alert (entrada + " Es propietario de la tienda " + tienda1.nombre);
+//     }
+//     if (tienda2.esPropietario(entrada)) {
+//         alert (entrada + " Es propietario de la tienda " + tienda2.nombre);
+//     }
+//     if (tienda3.esPropietario(entrada)) {
+//         alert (entrada + " Es propietario de la tienda " + tienda3.nombre);
+//     }
+// }
+
+// // ----------------------ACTIVIDAD 5
+// Declarar una clase Cliente que permita registrar:
+// Registrar nombre, presupuesto, si tiene tarjeta de descuento, y número de teléfono del cliente.
+
+// Un método transferirDinero(valor), que permita  obtener y restar cierta cantidad del presupuesto, siempre que este parámetro sea menor al valor enviado, y mayor que cero.
+
+// Luego invocar al menos tres (3) objetos usando esta clase, y solicitar al usuario cinco (5) números. Informar por alerta si cada uno de los clientes cuenta con el presupuesto suficiente para realizar una transferencia de igual monto al ingresado.
+
+// class Cliente {
+//     constructor(nombre, presupuesto, tarjeta, telefono) {
+//         this.nombre = nombre;
+//         this.presupuesto = presupuesto;
+//         this.tarjeta = tarjeta;
+//         this.telefono = telefono;
+//     }
+//     transferirDinero(valor) {
+//         if ((this.presupuesto > 0) && (valor < this.presupuesto)) {
+//             this.presupuesto -= valor;
+//             return valor;
+//         } else {
+//             return 0;
+//         }
+//     }
+// }
+// const cliente1 = new Cliente("Agustin", 1000, true, 513560156);
+// const cliente2 = new Cliente("Chano", "2000", false, 651651);
+// const cliente3 = new Cliente("Belu", "50", false, 1651651);
+// for (let i = 0; i < 5; i++) {
+//     let entrada = parseFloat(prompt("Ingresar monto"));
+//     if (cliente1.transferirDinero(entrada)) {
+//         alert("El cliente " + cliente1.nombre + " te puede pagar " + entrada);
+//     }
+//     if (cliente2.transferirDinero(entrada)) {
+//         alert("El cliente " + cliente2.nombre + " te puede pagar " + entrada);
+//     }
+//     if (cliente3.transferirDinero(entrada)) {
+//         alert("El cliente " + cliente3.nombre + " te puede pagar " + entrada);
+//     }
+// }
